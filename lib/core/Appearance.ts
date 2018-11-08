384dc99ac1a1872b8ddad6bee5d96ad191b856aa
Siamand Ma'roufi <siamandmaroufi@gmail.com>
2018-11-09 00:45:29 +0100

    group commands

--- lib/core/Appearance.ts
+++ lib/core/Appearance.ts
@@ -17,51 +17,74 @@ export enum BorderStyle {
 }
 
 export class Border {
-    public constructor(public color = null){
+    public constructor(public color = null) {
 
     }
 
-    public style:BorderStyle = BorderStyle.SolidThin;
+    public style: BorderStyle = BorderStyle.SolidThin;
 }
 
 export class Appearance {
-    public textAlign:TextAlign = TextAlign.Left;
-    public fontName:any;
-    public fontSize:any;
-    public textStyle:any;
-    public text:any;
-    public background:any;
-    public horisontalBorder:Border;
-    public verticalBorder:Border;
+    public textAlign: TextAlign = TextAlign.Left;
+    public fontName: any;
+    public fontSize: any;
+    public textStyle: any;
+    public text: any;
+    public background: any;
+    public horisontalBorder: Border;
+    public verticalBorder: Border;
 
     public get bold() {
-        return this.textStyle && this.textStyle.indexOf('bold') != -1;
+        return !!(this.textStyle && this.textStyle.indexOf('bold') != -1);
     }
 
-    public set bold(value:boolean) {
-        if(this.bold == value || !this.textStyle) return;
-        if(value) this.textStyle = `${this.textStyle} bold`;
-        else this.textStyle = this.textStyle.replace('bold','');
+    private ensureTextStyle() {
+        if (!this.textStyle) {
+            this.textStyle = '';
+        }
+    }
+
+    public set bold(value: boolean) {
+        this.ensureTextStyle();
+        if (this.bold == value) return;
+        if (value) this.textStyle = `${this.textStyle} bold`;
+        else this.textStyle = this.textStyle.replace('bold', '');
     }
 
     public get italic() {
-        return this.textStyle && this.textStyle.indexOf('italic') != -1;
+        return !!(this.textStyle && this.textStyle.indexOf('italic') != -1);
     }
 
-    public set italic(value:boolean) {
-        if(this.italic == value || !this.textStyle) return;
-        if(value) this.textStyle = `${this.textStyle} italic`;
-        else this.textStyle = this.textStyle.replace('italic','');
+    public set italic(value: boolean) {
+        this.ensureTextStyle();
+        if (this.italic == value || !this.textStyle) return;
+        if (value) this.textStyle = `${this.textStyle} italic`;
+        else this.textStyle = this.textStyle.replace('italic', '');
     }
 
     public get underline() {
-        return this.textStyle && this.textStyle.indexOf('underline') != -1;
+        return !!(this.textStyle && this.textStyle.indexOf('underline') != -1);
     }
 
-    public set underline(value:boolean) {
-        if(this.italic == value || !this.textStyle) return;
-        if(value) this.textStyle = `${this.textStyle} underline`;
-        else this.textStyle = this.textStyle.replace('underline','');
+    public set underline(value: boolean) {
+        this.ensureTextStyle();
+        if (this.italic == value || !this.textStyle) return;
+        if (value) this.textStyle = `${this.textStyle} underline`;
+        else this.textStyle = this.textStyle.replace('underline', '');
     }
-    
+
+    alignTextTo(value: string) {
+        if (value.toLowerCase() == "right") {
+            this.textAlign = TextAlign.Right;
+        }
+        else if (value.toLowerCase() == "center") {
+            this.textAlign = TextAlign.Center;
+        }
+        else if (value.toLowerCase() == "left") {
+            this.textAlign = TextAlign.Left;
+        } else {
+            throw `invalid text-alig '${value}'`
+        }
+
+    }
 }
--- lib/core/Commander.ts
+++ lib/core/Commander.ts
@@ -1,85 +1,93 @@
-import { OpenDocument } from "./Document";
-import { CommandHistory } from "./CommandHistory";
-import { Sheet } from "./Sheet";
-import { clone } from "../common/utils";
-import { Appearance, TextAlign } from "./Appearance";
+import {OpenDocument} from "./Document";
+import {CommandHistory} from "./CommandHistory";
+import {Sheet} from "./Sheet";
+import {clone} from "../common/utils";
+import {Appearance, TextAlign} from "./Appearance";
 
 export class Commander {
-    private history:CommandHistory[]=[];
-    private commands=[];
+    private history: CommandHistory[] = [];
+    private commands = [];
 
-    public constructor (private doc:OpenDocument) {
-        this.commands['bold']= () => this.bold();
-        this.commands['italic']= () => this.italic();
-        this.commands['underline']= () => this.underline();
-        this.commands['font-size']= (size) => this.fontSize(size);
-        this.commands['font-name']= (size) => this.fontName(size);
-        this.commands['bg-color'] = (color) => this.bgColor(color);
-        this.commands['fg-color'] = (color) => this.fgColor(color);
+    public constructor(private doc: OpenDocument) {
+        this.commands['bold'] = () => this.appearance(app => app.bold = !app.bold);
+        this.commands['italic'] = () => this.appearance(app => app.italic = !app.italic);
+        this.commands['underline'] = () => this.appearance(app => app.underline = !app.underline);
+        this.commands['font-size'] = (size) => this.appearance(app => app.fontSize = size);
+        this.commands['font-name'] = (font) => this.appearance(app => app.fontName = font);
+        this.commands['bg-color'] = (color) => this.appearance(app => app.background = color);
+        this.commands['fg-color'] = (color) => this.appearance(app => app.text = color);
         this.commands['merge'] = () => this.merge();
         this.commands['unmerge'] = () => this.unmerge();
-        this.commands['align'] = (value) => this.align(value);
+        this.commands['align'] = (value) => this.appearance(app => app.alignTextTo(value));
         this.commands['change-value'] = (columnId, rowId, value) => this.changeValue(columnId, rowId, value);
         this.commands['alter-column'] = (columnId, delta) => this.alterColumn(columnId, delta);
         this.commands['alter-row'] = (rowId, delta) => this.alterRow(rowId, delta);
     }
 
-    private has (commandName){
+    private has(commandName) {
         return !!this.commands[commandName];
     }
 
-    do (commandName,...args){
-        if(!this.has(commandName)){
+    do(commandName, ...args) {
+        if (!this.has(commandName)) {
             console.warn(`command ${commandName} does not exsits!`);
         }
 
         this.commands[commandName](...args);
     }
 
-    private get ActiveSheet():Sheet{
+    private get ActiveSheet(): Sheet {
         return this.doc.ActiveSheet;
     }
 
-    private get Selection () {
+    private get Selection() {
         return this.ActiveSheet.selection;
     }
 
-    private get SelectedAppearance ():Appearance {
+    private get SelectedAppearance(): Appearance [] {
         let sel = this.Selection;
         let sheet = this.doc.ActiveSheet;
-        return clone(sheet.getCellApperance(sel.columnId,sel.rowId));
+        let apps = [];
+        for (let r = sel.top; r <= sel.bottom; r++) {
+            for (let c = sel.left; c <= sel.right; c++) {
+                let app = sheet.getCellApperance(sel.columnId, sel.rowId);
+                if (app) {
+                    apps.push(clone(app));
+                }
+            }
+        }
+        return apps;
     }
 
-    private get SelectionData () {
+    private get SelectionData() {
         let sel = this.Selection;
         let sheet = this.doc.ActiveSheet;
         let data = [];
 
-        for(let c=sel.left;c <= sel.right;c++) { 
-            for(let r=sel.top;r<= sel.bottom;r++) {
-                let cell = sheet.getCell(c,r);
+        for (let c = sel.left; c <= sel.right; c++) {
+            for (let r = sel.top; r <= sel.bottom; r++) {
+                let cell = sheet.getCell(c, r);
                 data.push(clone(cell));
-
             }
         }
 
         return data;
     }
 
-    private logAppearanceOnlyCommnand () {
+    private logAppearanceOnlyCommnand() {
         let sel = this.Selection;
-        let historyItem:CommandHistory = {
-            selection:sel,
-            appearance:this.SelectedAppearance
+        let historyItem: CommandHistory = {
+            selection: sel,
+            appearance: this.SelectedAppearance
         };
 
         this.history.push(historyItem);
     }
 
-    private logDataOnlyCommand () {
+    private logDataOnlyCommand() {
         let sel = this.Selection;
-        let historyItem:CommandHistory = {
-            selection:sel,
+        let historyItem: CommandHistory = {
+            selection: sel,
             data: this.SelectionData
         };
 
@@ -89,7 +97,7 @@ export class Commander {
     private alterColumn(column, delta) {
         const w = this.doc.ActiveSheet.getColumnWidth(column);
         this.history.push({
-            selection:column,
+            selection: column,
             data: w,
             meta: 'COLUMN'
         });
@@ -100,7 +108,7 @@ export class Commander {
     private alterRow(row, delta) {
         const h = this.doc.ActiveSheet.getRowHeight(row);
         this.history.push({
-            selection:row,
+            selection: row,
             data: h,
             meta: 'ROW'
         });
@@ -110,108 +118,44 @@ export class Commander {
 
     private changeValue(columnId, rowId, value) {
         this.logDataOnlyCommand();
-        if(columnId == null) columnId = this.Selection.columnId;
-        if(rowId == null) rowId = this.Selection.rowId;
+        if (columnId == null) columnId = this.Selection.columnId;
+        if (rowId == null) rowId = this.Selection.rowId;
         this.ActiveSheet.setCellValue(columnId, rowId, value);
     }
 
-    private merge () {
+    private merge() {
         this.logDataOnlyCommand();
         let sel = this.Selection;
         let c = Math.min(sel.right, sel.left);
         let r = Math.min(sel.top, sel.bottom);
         let w = Math.abs(sel.right - sel.left) + 1;
         let h = Math.abs(sel.top - sel.bottom) + 1;
-        this.ActiveSheet.merge(c, r, w,h);
+        this.ActiveSheet.merge(c, r, w, h);
     }
 
-    private unmerge () {
+    private unmerge() {
         this.logDataOnlyCommand();
         let sel = this.Selection;
         this.ActiveSheet.unmerge(sel.columnId, sel.rowId);
     }
 
-    private toggle (str,t) {
-        str = str || '';
-        if(str.indexOf(t) != -1) {
-            return str.replace(t, '')
+    private appearance(method: (app: Appearance) => void): void {
+        this.logAppearanceOnlyCommnand();
+        let sel = this.Selection;
+        for (let r = sel.top; r <= sel.bottom; r++) {
+            for (let c = sel.left; c <= sel.right; c++) {
+                let app = this.ActiveSheet.getCellApperance(c, r) || new Appearance();
+                method(app);
+                this.ActiveSheet.setCellAppearance(c, r, app);
+            }
         }
-        return `${str} ${t}`;
     }
 
-
-    private align (value) {
-        this.logAppearanceOnlyCommnand();
-        let sel = this.Selection;
-        let app = this.SelectedAppearance || new Appearance();
-        if(value=='right') app.textAlign = TextAlign.Right;
-        else if(value=='left') app.textAlign = TextAlign.Left;
-        else if(value=='center') app.textAlign = TextAlign.Center;
-        else throw `${value} is not a valid value`;
-        this.ActiveSheet.setCellAppearance(sel.columnId, sel.rowId, app);
-    }
-
-    private bold () {
-        this.logAppearanceOnlyCommnand();
-        let sel = this.Selection;
-        let app = this.SelectedAppearance || new Appearance();
-        app.textStyle = this.toggle(app.textStyle, ' bold');
-        this.ActiveSheet.setCellAppearance(sel.columnId, sel.rowId, app);
-    }
-
-    private italic () {
-        this.logAppearanceOnlyCommnand();
-        let sel = this.Selection;
-        let app = this.SelectedAppearance || new Appearance();
-        app.textStyle = this.toggle(app.textStyle, ' italic');
-        this.ActiveSheet.setCellAppearance(sel.columnId, sel.rowId, app);
-    }
-
-    private underline () {
-        this.logAppearanceOnlyCommnand();
-        let sel = this.Selection;
-        let app = this.SelectedAppearance || new Appearance();
-        app.textStyle = this.toggle(app.textStyle, ' underline');
-        this.ActiveSheet.setCellAppearance(sel.columnId, sel.rowId, app);
-    }
-
-    private bgColor (color) {
-        this.logAppearanceOnlyCommnand();
-        let sel = this.Selection;
-        let app = this.SelectedAppearance || new Appearance();
-        app.background = color;
-        this.ActiveSheet.setCellAppearance(sel.columnId, sel.rowId, app);
-    }
-
-    private fgColor (color) {
-        this.logAppearanceOnlyCommnand();
-        let sel = this.Selection;
-        let app = this.SelectedAppearance || new Appearance();
-        app.text = color;
-        this.ActiveSheet.setCellAppearance(sel.columnId, sel.rowId, app);
-    }
-
-    private fontSize(size) {
-        this.logAppearanceOnlyCommnand();
-        let sel = this.Selection;
-        let app = this.SelectedAppearance || new Appearance();
-        app.fontSize = size;
-        this.ActiveSheet.setCellAppearance(sel.columnId, sel.rowId, app);
-    }
-
-    private fontName(size) {
-        this.logAppearanceOnlyCommnand();
-        let sel = this.Selection;
-        let app = this.SelectedAppearance || new Appearance();
-        app.fontName = size;
-        this.ActiveSheet.setCellAppearance(sel.columnId, sel.rowId, app);
-    }
-
-    public undo(){
+    public undo() {
         throw 'undo has not been implemented';
     }
 
-    public redo(){
+    public redo() {
         throw 'redo has not been implemented'
     }
 
