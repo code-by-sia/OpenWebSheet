f30fb1c94dc0528b9bac434a75548bb89f2311c6
cyaxares <me@siamand.cc>
2019-12-07 00:59:29 +0100

    feat: PWA support

---
+++ docs/js/service-worker.js
@@ -0,0 +1,10 @@
+
+self.addEventListener('install', function () {
+    console.log('Install!');
+});
+self.addEventListener("activate", event => {
+    console.log('Activate!');
+});
+self.addEventListener('fetch', function (event) {
+    console.log('Fetch!', event.request);
+});
