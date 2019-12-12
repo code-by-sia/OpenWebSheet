d745d350b2bb4d445711f606bbf147a9aa23a794
Siamand Maroufi <siamandmaroufi@gmail.com>
2019-12-13 00:11:24 +0100

    fix

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.697f6b5602b476f6ff17ab966d595b6e.js"
+  "precache-manifest.697f6b5602b476f6ff17ab966d595b6e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "openwebsheet"});
